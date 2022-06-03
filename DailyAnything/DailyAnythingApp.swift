5981e709e06712e4361db79535237443491dfd1a
Jared Salzano <jsalzano@apple.com>
2022-06-03 11:20:08 -0400

    Initial Commit

---
+++ DailyAnything/DailyAnythingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyAnythingApp.swift
+//  DailyAnything
+//
+//  Created by Jared Salzano on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct DailyAnythingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
